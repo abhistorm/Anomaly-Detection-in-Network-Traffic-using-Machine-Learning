bb0f4cf9eb935fa8f37604983f0cb1adc374479e
abhistorm <74293846+abhistorm@users.noreply.github.com>
2025-02-24 21:44:12 +0530

    first commit

---
+++ DecisionTree.py
@@ -0,0 +1,89 @@
+import numpy as np
+import pandas as pd
+from sklearn.kernel_approximation import RBFSampler
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.metrics import classification_report
+from sklearn import metrics
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import (precision_score, recall_score,f1_score, accuracy_score,mean_squared_error,mean_absolute_error)
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import preprocessing
+from sklearn.preprocessing import Normalizer
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import (precision_score, recall_score,f1_score, accuracy_score,mean_squared_error,mean_absolute_error, roc_curve, classification_report,auc)
+import warnings
+warnings.filterwarnings("ignore")
+traindata = pd.read_csv('Dataset/tr.csv', header=None)
+testdata = pd.read_csv('Dataset/ts.csv', header=None)
+
+X = traindata.iloc[:,1:42]
+Y = traindata.iloc[:,0]
+C = testdata.iloc[:,0]
+T = testdata.iloc[:,1:42]
+
+scaler = Normalizer().fit(X)
+trainX = scaler.transform(X)
+
+scaler = Normalizer().fit(T)
+testT = scaler.transform(T)
+
+
+traindata = np.array(trainX)
+trainlabel = np.array(Y)
+
+testdata = np.array(testT)
+testlabel = np.array(C)
+
+
+
+
+
+
+
+
+
+print("***************************************************************")
+
+
+
+
+
+model = DecisionTreeClassifier()
+model.fit(traindata, trainlabel)
+print(model)
+# make predictions
+expected = testlabel
+predicted = model.predict(testdata)
+np.savetxt('predictedDT.txt', predicted, fmt='%01d')
+# summarize the fit of the model
+accuracy = accuracy_score(expected, predicted)
+recall = recall_score(expected, predicted, average="binary")
+precision = precision_score(expected, predicted , average="binary")
+f1 = f1_score(expected, predicted , average="binary")
+
+cm = metrics.confusion_matrix(expected, predicted)
+print(cm)
+tpr = float(cm[0][0])/np.sum(cm[0])
+fpr = float(cm[1][1])/np.sum(cm[1])
+print("%.3f" %tpr)
+print("%.3f" %fpr)
+print("Accuracy")
+print("%.3f" %accuracy)
+print("precision")
+print("%.3f" %precision)
+print("recall")
+print("%.3f" %recall)
+print("f-score")
+print("%.3f" %f1)
+
+print("***************************************************************")
+
+
